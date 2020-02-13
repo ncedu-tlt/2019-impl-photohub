a46fc22ed1e2af6ede683fc8e5cc0b25f2abcc4c
IvanVoevodin <voevodin36@gmail.com>
2020-02-13 10:31:55 +0400

    Show users from database

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to demo page for PhotoHub app"/>
   </div>
 </template>
 
---
+++ client/src/components/Users.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h3>Users stored in database</h3>
+        <ul>
+            <li v-for="user in users"
+                :key="user.id">
+                {{user.name}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import axios from "axios"
+
+    const Users = {
+        name: "Users",
+
+        data: function () {
+            return {
+                users: []
+            }
+        },
+
+        mounted() {
+            axios.get("/demo/all")
+                .then(response => {
+                    this.users = response.data
+                })
+                .catch(error => {
+                    console.error(error);
+                })
+        }
+    };
+
+    export default Users
+</script>
+
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        margin: 10px;
+    }
+</style>
