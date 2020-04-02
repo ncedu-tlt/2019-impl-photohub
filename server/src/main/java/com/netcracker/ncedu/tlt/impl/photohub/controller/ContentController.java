7e321ce544d570ab7dbb0bdfb2762448d7da591b
DOK4ic <artem4ic51@gmail.com>
2020-04-02 15:33:12 +0400

    Added ContentFeed(ContentController) Added avatar Image(Avatar)

--- server/src/main/java/com/netcracker/ncedu/tlt/impl/photohub/controller/ContentController.java
+++ server/src/main/java/com/netcracker/ncedu/tlt/impl/photohub/controller/ContentController.java
@@ -1,7 +1,7 @@
 package com.netcracker.ncedu.tlt.impl.photohub.controller;
 
-import com.netcracker.ncedu.tlt.impl.photohub.model.ContentRepository;
 import com.netcracker.ncedu.tlt.impl.photohub.model.Photo;
+import com.netcracker.ncedu.tlt.impl.photohub.model.PhotoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +11,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+
 
 @Controller
 @RequestMapping(path="/api/contentfeed")
 public class ContentController {
     @Autowired
-    private ContentRepository contentRepository;
+    private PhotoRepository photoRepository;
 
     @GetMapping(path = "/get_content")
     @ResponseBody
     public Object getImagesById(@RequestParam Integer id, String email) throws IOException {
-        List<String> images = contentRepository.findByEmailAndId(id)
-                .stream()
-                .map(Photo::getBase64)
-                .collect(Collectors.toList());
+        List<String> images = new LinkedList<>();
+        for (Photo photo : photoRepository.findAll()) {
+            images.add(photo.getBase64());
+        }
         Map<String, List<String>> map = new HashMap<>();
         map.put("images", images);
         return map;
